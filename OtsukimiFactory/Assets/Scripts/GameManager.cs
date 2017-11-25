aead1d9d42536a78efafb52b54de96f0f40a26d2
Yujiro Ito <yujiro.bear@gmail.com>
2017-11-25 14:44:28 +0900

    GameManagerに時間計算処理を実装

--- OtsukimiFactory/Assets/Scripts/GameManager.cs
+++ OtsukimiFactory/Assets/Scripts/GameManager.cs
@@ -10,19 +10,34 @@ public class GameManager : MonoBehaviour {
 	private WorkArea[] _workAreas;
 	private float _time;
 	private int _score;
+	private bool _finish;
 
 	//---propaties---
 	public WorkArea[] WorkAreas{ get{ return _workAreas; }}
+	public bool Finish{ get{ return _finish; }}
 
 	// Use this for initialization
 	void Start () {
 		_time = TIME_MAX;
 		_score = 0;
+		_finish = false;
 		_workAreas = GameObject.FindObjectsOfType<WorkArea>();
+
+		//コルーチンの再生
+		StartCoroutine(TimeCounter());
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+	}
+
+	//時間計算用コルーチン
+	private IEnumerator TimeCounter(){
+		while(_time > 1){
+			yield return new WaitForSeconds(1);
+			_time--;
+		}
+		_finish = true;
 	}
 }
